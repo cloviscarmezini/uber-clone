106d40a4c6f36ed92a8b66be78c201e508925601
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-03-24 18:51:53 -0300

    chore: configure initial project

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#000'
+      }}>
+        <Text
+          style={{
+            color: '#FFF',
+            fontSize: 36
+          }}
+        >
+          <Text
+            style={{
+              fontWeight: 'bold',
+              fontSize: 48
+            }}
+          >
+            Uber
+          </Text>
+          Clone
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
