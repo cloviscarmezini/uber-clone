31c32cf2d1ae4c4a1ef228fb90ada6ede40441ce
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-03-24 19:51:07 -0300

    feat: add taiwind css

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,33 +1,13 @@
 import React from "react";
 
-import { Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 
 export function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#000",
-      }}
-    >
-      <Text
-        style={{
-          color: "#FFF",
-          fontSize: 36,
-        }}
-      >
-        <Text
-          style={{
-            fontWeight: "bold",
-            fontSize: 48,
-          }}
-        >
-          Uber
-        </Text>
-        Clone
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text>
+        Uber Clone
       </Text>
-    </View>
+    </SafeAreaView>
   );
 }
