671228460fb843b1b836e4c60da00efe97ed2000
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-03-25 15:52:02 -0300

    feat: add react-native-elements

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'react-redux';
 import { Home } from './src/screens/Home';
 import { store } from './src/store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Home />
-      <StatusBar style="auto" />
+      <SafeAreaProvider>
+        <Home />
+        <StatusBar style="auto" />
+      </SafeAreaProvider>
     </Provider>
   );
 }
