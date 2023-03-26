00ecc5768b9154abd89a2f2e63abcaa4cfdb429c
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-03-25 21:47:59 -0300

    feat: add react-navigation

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'react-redux';
-import { Home } from './src/screens/Home';
 import { store } from './src/store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import { Home } from './src/screens/Home';
+import { Map } from './src/screens/Map';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const { Navigator, Screen } = createNativeStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <Home />
-        <StatusBar style="auto" />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Navigator
+            screenOptions={{
+              headerShown: false
+            }}
+          >
+            <Screen
+              name='home'
+              component={Home}
+            />
+            <Screen
+              name='map'
+              component={Map}
+            />
+          </Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
