8c79e05e98ca5d7905b7e12adca362f7e1a52ff0
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-03-25 21:48:44 -0300

    feat: create map page

---
+++ src/screens/Map.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { SafeAreaView, Text, View } from 'react-native';
+
+export function Map() {
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <View className="p-5">
+        <Text>
+          there's a map here
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
