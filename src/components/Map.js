eff69f8eb81c490fcfb42a5602231d4440e1e378
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-03-26 16:25:51 -0300

    feat: created map component

---
+++ src/components/Map.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import MapView, { Marker } from "react-native-maps";
+
+import { useSelector } from "react-redux";
+import { selectOrigin } from "../slices/navSlice";
+
+export function Map() {
+  const origin = useSelector(selectOrigin);
+
+  return (
+    <MapView
+      className="flex-1"
+      mapType="mutedStandard"
+      initialRegion={{
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}
+    >
+        { origin.location && (
+            <Marker
+                coordinate={{
+                    latitude: origin.location.lat,
+                    longitude: origin.location.lng
+                }}
+            />
+        )}
+    </MapView>
+  );
+}
