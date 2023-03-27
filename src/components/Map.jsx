d9e86ce22eb78630b39ba0fe6e42857e2c42713d
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-03-27 10:17:15 -0300

    feat: fit map to markers on select destination

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import MapView, { Marker } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
@@ -8,14 +8,30 @@ import { selectDestination, selectOrigin } from "../slices/navSlice";
 import { getCoordinates } from "../utils/functions";
 
 export function Map() {
+  const mapRef = useRef(null);
+
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
 
   const originCoordinates = origin.location ? getCoordinates(origin.location) : null;
   const destinationCoordinates = destination?.location ? getCoordinates(destination.location) : null;
 
+  useEffect(() => {
+    if(!origin || !destination) return;
+
+    mapRef.current.fitToSuppliedMarkers(['origin', 'destination']), {
+      edgePadding: {
+        top: 50,
+        right: 50,
+        bottom: 50,
+        left: 50
+      }
+    };
+  }, [origin, destination])
+
   return (
     <MapView
+      ref={mapRef}
       className="flex-1"
       mapType="mutedStandard"
       initialRegion={{
@@ -36,11 +52,13 @@ export function Map() {
       { origin.location && (
         <Marker
             coordinate={originCoordinates}
+            identifier="origin"
         />
       )}
       { destination?.location && (
         <Marker
           coordinate={destinationCoordinates}
+          identifier="destination"
         />
       )}
     </MapView>
