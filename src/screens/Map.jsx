f9700ae11fc78e69be15638cd204bdc7d20dc58e
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-03-27 14:05:54 -0300

    feat: add menu button to map screen

--- src/screens/Map.jsx
+++ src/screens/Map.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 
-import { View } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import { Map } from "../components/Map";
 import { NavigateCard } from "../components/NavigateCard";
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RideOptionsCard } from "../components/RideOptionsCard";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export function MapScreen() {
+  const navigation = useNavigation();
+
   return (
     <View>
+      <TouchableOpacity
+        className="absolute top-16 z-50 left-8 bg-gray-100 rounded-full p-3 shadow-lg"
+        onPress={() => navigation.navigate('home')}
+      >
+        <Icon
+          color="black"
+          name="menu"
+          type="fontawesome"
+        />
+      </TouchableOpacity>
+
       <View className="h-1/2">
         <Map />
       </View>
